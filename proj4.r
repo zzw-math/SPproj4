8e1724437fe09fd9b23445e69e7b0f999ff70e46
TianqiD <113841006+TqD7@users.noreply.github.com>
2022-11-10 16:38:12 +0000

    add input comment

--- proj4.r
+++ proj4.r
@@ -1,7 +1,7 @@
 ##      names     | university user names
 ##   Ziwen Zhong  | s2326022
 ## Wenzheng Zhang |
-##   Tianqi Dai   |
+##   Tianqi Dai   | s2302524
 
 ## github repo: https://github.com/zzw-math/SPproj4.git
 
@@ -10,11 +10,24 @@
 
 #####
 ## summary
-## 
+## Project 4 Overview: In this project, we are going to define a function newt.
 #####
 
 newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,
                  max.half=20,eps=1e-6) {
+  ## function newt with default data  
+  ## Input: theta, vector of the initial values of optimising parameters
+  ##        func, the objective function to be minimised
+  ##        grad, the gradient vector returned by the gradient function
+  ##        hess,  the Hessian matrix function
+  ##        tol, the convergence tolerance
+  ##        fscale, estimation of the magnitude of func near the optimum
+  ##        maxit, maximum number of Newton iterations
+  ##        max.half,maximum times to halve a step before the deduction of
+  ##                failure on objective improvement
+  ##        eps, the finite difference intervals to use when a Hessian function 
+  ##                is not provided 
+  
   iter <- 1
   func_value <- func_value_original <- func(theta,...)
   grad_vector <- grad(theta,...)
